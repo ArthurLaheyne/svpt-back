59c288b757ccf8f1babba618308b91efee521d12
Arthur Laheyne <arthur.laheyne@gmail.com>
2018-06-09 22:13:03 +0200

    db

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -16,5 +22,16 @@ express()
       result += i + ' '
     }
     res.send(result)
+  })
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      res.render('pages/db', result);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
