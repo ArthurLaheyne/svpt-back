37370cdb0f255e21657b7618ca89e6e5e30fb26a
Arthur Laheyne <arthur.laheyne@gmail.com>
2018-06-10 18:01:53 +0200

    connexion à mongoDB/joueur

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MONGODB_URI;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,13 +10,23 @@ router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
 
   // And insert something like this instead:
-  res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);
+  MongoClient.connect(url, function (err, client) {
+    if (err) throw err
+
+    var db = client.db('heroku_48jsz1bx')
+
+    db.collection('joueur').find().toArray(function (err, result) {
+      if (err) throw err
+      res.json(result);
+    })
+    // res.json([{
+    // 	id: 1,
+    // 	username: "samsepi0l"
+    // }, {
+    // 	id: 2,
+    // 	username: "D0loresH4ze"
+    // }]);
+  })
 });
 
 module.exports = router;
