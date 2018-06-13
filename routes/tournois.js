89d7e887a1a8687cb6da377641840b637e00ea99
Arthur LAHEYNE <a.laheyne@acat.fr>
2018-06-13 11:16:13 +0200

    l'api tournois retournent les users avec

--- routes/tournois.js
+++ routes/tournois.js
@@ -15,17 +15,26 @@ router.get('/', function(req, res, next) {
 
     var db = client.db('heroku_48jsz1bx')
 
-    db.collection('tournoi').find().toArray(function (err, result) {
-      if (err) throw err
-      res.json(result);
+    db.collection('joueur').find().toArray(function (err, joueurs) {
+      if (err)
+        throw err
+      else {
+        db.collection('tournoi').find().toArray(function (err, tournois) {
+          if (err)
+            throw err
+          else {
+            tournois.forEach( (tournoi, i) => {
+              tournoi.resultat.forEach( (resultat, i2) => {
+                tournois[i].resultat[i2].joueur = joueurs.find( (element) => {
+                  return element._id == resultat.joueur;
+                })
+              })
+            })
+            res.json(tournois);
+          }
+        })
+      }
     })
-    // res.json([{
-    // 	id: 1,
-    // 	username: "samsepi0l"
-    // }, {
-    // 	id: 2,
-    // 	username: "D0loresH4ze"
-    // }]);
   })
 });
 
